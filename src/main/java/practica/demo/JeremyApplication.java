7a9b9ae3ffea8b6f7604747476212ad5f822de22
jrg71 <jrg71@JeremyPC>
2023-06-03 17:24:32 -0600

    Primer commit.

---
+++ src/main/java/practica/demo/JeremyApplication.java
@@ -0,0 +1,13 @@
+package practica.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeremyApplication.class, args);
+	}
+
+}
